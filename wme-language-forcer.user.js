24be8944e03c92785b18dc80fd447b0b7761b712
tunisiano187 <fabian@bowlman.org>
2018-07-18 20:24:33 +0000

    autoupdate

--- wme-language-forcer.user.js
+++ wme-language-forcer.user.js
@@ -4,6 +4,12 @@
 // @description Script to redirect to your language automaticaly
 // @include     /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor([^\/].*)?$/
 // @version     0.1
+// @author         tunisiano187 '2018 based on Rick Zabel '2014
+// @license        MIT/BSD/X11
+// @updateURL      https://gitlab.com/WMEScripts/WME-language-forcer/raw/master/wme-language-forcer.user.js
+// @compatible     chrome firefox
+// @supportURL      mailto:incoming+WMEScripts/WME-language-forcer@incoming.gitlab.com
+// @contributionURL https://flattr.com/@tunisiano
 // @grant       none
 // ==/UserScript==
 
