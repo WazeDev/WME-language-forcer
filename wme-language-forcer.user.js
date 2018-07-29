52a2a1b96fe4bf2343e6400ecaf8c9c4ae003a2f
tunisiano187 <fabian@bowlman.org>
2018-07-29 13:43:43 +0000

    1.6 - intercept error and ask user for new language

--- wme-language-forcer.user.js
+++ wme-language-forcer.user.js
@@ -8,7 +8,7 @@
 // @match          https://editor-beta.waze.com/*editor/*
 // @match          https://beta.waze.com/*editor/*
 // @match          https://www.waze.com/*/editor/*
-// @version        1.5
+// @version        1.6
 // @author         tunisiano187 '2018
 // @license        MIT/BSD/X11
 // @compatible     chrome firefox
@@ -16,13 +16,22 @@
 // @contributionURL http://ko-fi.com/tunisiano
 // @grant          none
 // ==/UserScript==
+var language = "";
+var ts = Math.round((new Date()).getTime() / 1000);
 if(window.location.hash == ("#reset-WME-prefered-language")) {
     localStorage.removeItem('WME-prefered-language');
     alert("Language resetted");
     window.location.href = "https://www.waze.com/editor/";
 }
-var language = "";
-if('WME-prefered-language' in localStorage) {
+else if($(".not-found")[0]) {
+    if ('WME-prefered-language-last-refresh' in localStorage){
+        prevts = localStorage.getItem('WME-prefered-language-last-refresh');
+        if(Math.round(ts-5)>prevts) {
+            window.location.href = "https://www.waze.com/editor/#reset-WME-prefered-language";
+        }
+    }
+  }
+else if('WME-prefered-language' in localStorage) {
     language = localStorage.getItem('WME-prefered-language');
   }
 else
@@ -39,6 +48,7 @@ if (location.pathname.indexOf("/" + language + "/editor") !== 0 && location.path
       {
           fullpath = fullpath.substring(1);
       }
+      localStorage.setItem('WME-prefered-language-last-refresh',ts);
     window.location.replace = window.location.replace('https://www.waze.com/' + language + fullpath);
   }, 1000);
 }
